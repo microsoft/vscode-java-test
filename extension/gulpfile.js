81f05b47e35ad314f1bd7e91876facfc5234eb08
Xuan Zhou <xuzho@microsoft.com>
2017-12-28 14:39:41 +0800

    report support theme dark/light (#71)
    Signed-off-by: xuzho <xuzho@microsoft.com>

--- extension/gulpfile.js
+++ extension/gulpfile.js
@@ -1,34 +1,40 @@
 const gulp = require('gulp');
 const cp = require('child_process');
 const tslint = require("gulp-tslint");
+const sass = require('gulp-sass');
 const server_dir = '../';
-gulp.task('build_server', ()=>
-{
-	cp.execSync(mvnw()+ ' clean package', {cwd:server_dir, stdio:[0,1,2]} );
-	gulp.src(server_dir + '/com.microsoft.java.test.plugin/target/*.jar')
+gulp.task('build_server', () => {
+    cp.execSync(mvnw() + ' clean package', { cwd: server_dir, stdio: [0, 1, 2] });
+    gulp.src(server_dir + '/com.microsoft.java.test.plugin/target/*.jar')
         .pipe(gulp.dest('./server'));
     gulp.src(server_dir + '/com.microsoft.java.test.runner/target/*.jar')
-		.pipe(gulp.dest('./server'))
+        .pipe(gulp.dest('./server'))
 });
 
 gulp.task("tslint", () => {
-	return gulp.src(["**/*.ts", "!**/*.d.ts", "!node_modules/**", "!./src/views/node_modules/**"])
-		.pipe(tslint())
-		.pipe(tslint.report());
+    return gulp.src(["**/*.ts", "!**/*.d.ts", "!node_modules/**", "!./src/views/node_modules/**"])
+        .pipe(tslint())
+        .pipe(tslint.report());
 });
 
+gulp.task('sass', () => {
+    return gulp.src(['resources/templates/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("resources/templates/css"));
+})
+
 function isWin() {
-	return /^win/.test(process.platform);
+    return /^win/.test(process.platform);
 }
 
 function isMac() {
-	return /^darwin/.test(process.platform);
+    return /^darwin/.test(process.platform);
 }
 
 function isLinux() {
-	return /^linux/.test(process.platform);
+    return /^linux/.test(process.platform);
 }
 
 function mvnw() {
-	return isWin()?"mvnw.cmd":"./mvnw";
+    return isWin() ? "mvnw.cmd" : "./mvnw";
 }
