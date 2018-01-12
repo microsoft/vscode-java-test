47f5de5c17b0f272a223e18e2fc856b16ff41ef0
Xuan Zhou <xuzho@microsoft.com>
2018-01-12 19:09:23 +0800

    walkaround for jdt bug: getAnnotaion would return non-existed annotation (#92)
    Signed-off-by: xuzho <xuzho@microsoft.com>

--- com.microsoft.java.test.plugin/src/main/java/com/microsoft/java/test/plugin/internal/JUnitUtility.java
+++ com.microsoft.java.test.plugin/src/main/java/com/microsoft/java/test/plugin/internal/JUnitUtility.java
@@ -23,49 +23,63 @@ import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 
 public class JUnitUtility {
-	public static boolean isTestMethod(IMethod method, String annotation) {
-		int flags;
-		try {
-			flags = method.getFlags();
-			// 'V' is void signature
-			return !(method.isConstructor() || !Flags.isPublic(flags) || Flags.isAbstract(flags) || Flags.isStatic(flags) || !"V".equals(method.getReturnType())) && method.getAnnotation(annotation).exists();
-		} catch (JavaModelException e) {
-			// ignore
-			return false;
-		}
-	}
-	
-	public static boolean isTestClass(IType type, String annotation) {
-		try {
-			if (!isAccessibleClass(type)) {
-				return false;
-			}
-			if (Flags.isAbstract(type.getFlags())) {
-				return false;
-			}
-			List<IMethod> tests = Arrays.stream(type.getMethods()).filter(m -> isTestMethod(m, annotation)).collect(Collectors.toList());
-			return tests.size() > 0;
-		} catch (JavaModelException e) {
-			return false;
-		}
-		
-	}
-	
-	public static boolean isAccessibleClass(IType type) throws JavaModelException {
-	    int flags = type.getFlags();
-	    if (Flags.isInterface(flags)) {
-	      return false;
-	    }
-	    IJavaElement parent = type.getParent();
-	    while (true) {
-	      if (parent instanceof ICompilationUnit || parent instanceof IClassFile) {
-	        return true;
-	      }
-	      if (!(parent instanceof IType) || !Flags.isStatic(flags) || !Flags.isPublic(flags)) {
-	        return false;
-	      }
-	      flags = ((IType) parent).getFlags();
-	      parent = parent.getParent();
-	    }
-	  }
+    public static boolean isTestMethod(IMethod method, String annotation) {
+        int flags;
+        try {
+            flags = method.getFlags();
+            // 'V' is void signature
+            return !(method.isConstructor() || !Flags.isPublic(flags) || Flags.isAbstract(flags)
+                    || Flags.isStatic(flags) || !"V".equals(method.getReturnType()))
+                    && hasTestAnnotation(method, annotation);
+        } catch (JavaModelException e) {
+            // ignore
+            return false;
+        }
+    }
+
+    public static boolean isTestClass(IType type, String annotation) {
+        try {
+            if (!isAccessibleClass(type)) {
+                return false;
+            }
+            if (Flags.isAbstract(type.getFlags())) {
+                return false;
+            }
+            List<IMethod> tests = Arrays.stream(type.getMethods()).filter(m -> isTestMethod(m, annotation))
+                    .collect(Collectors.toList());
+            return tests.size() > 0;
+        } catch (JavaModelException e) {
+            return false;
+        }
+
+    }
+
+    public static boolean isAccessibleClass(IType type) throws JavaModelException {
+        int flags = type.getFlags();
+        if (Flags.isInterface(flags)) {
+            return false;
+        }
+        IJavaElement parent = type.getParent();
+        while (true) {
+            if (parent instanceof ICompilationUnit || parent instanceof IClassFile) {
+                return true;
+            }
+            if (!(parent instanceof IType) || !Flags.isStatic(flags) || !Flags.isPublic(flags)) {
+                return false;
+            }
+            flags = ((IType) parent).getFlags();
+            parent = parent.getParent();
+        }
+    }
+
+    static boolean hasTestAnnotation(IMethod method, String annotation) {
+        try {
+            if (!Arrays.stream(method.getAnnotations()).anyMatch(a -> a.getElementName().equals(annotation))) {
+                return false;
+            }
+            return method.getAnnotation(annotation).exists();
+        } catch (JavaModelException e) {
+            return false;
+        }
+    }
 }
