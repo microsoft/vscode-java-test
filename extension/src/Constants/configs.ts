ed6abf58e421aca973104ab7f1033fe74934092c
xuzho <xuzho@microsoft.com>
2018-03-27 16:34:55 +0800

    configII: add commands
    Signed-off-by: xuzho <xuzho@microsoft.com>

--- extension/src/Constants/configs.ts
+++ extension/src/Constants/configs.ts
@@ -4,3 +4,5 @@
 export const LANGUAGE = 'java';
 export const LOG_FILE_NAME = 'java_test_runner.log';
 export const MAX_CLASS_PATH_LENGTH = 4096;
+export const TEST_LAUNCH_CONFIG_NAME = 'test-launch.config';
+export const CHILD_PROCESS_MAX_BUFFER_SIZE = 1024 * 1024;
