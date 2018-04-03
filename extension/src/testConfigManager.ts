d6c0b237f2ac327d42d94ae5050dcba48626f379
Xuan Zhou <xuzho@microsoft.com>
2018-04-03 13:16:33 +0800

    change test config storage path from extension folder to storage folder (#159)
    * change test config storage path from extension folder to storage folder
    
    Signed-off-by: xuzho <xuzho@microsoft.com>
    
    * fix tslint
    
    Signed-off-by: xuzho <xuzho@microsoft.com>
    
    * move config file to .vscode folder and change logic about edit configuration
    
    Signed-off-by: xuzho <xuzho@microsoft.com>
    
    * resolve comments
    
    Signed-off-by: xuzho <xuzho@microsoft.com>

--- extension/src/testConfigManager.ts
+++ extension/src/testConfigManager.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 import * as fs from 'fs';
+import * as mkdirp from 'mkdirp';
 import * as path from 'path';
 import { window, workspace, ExtensionContext, TextEditor } from 'vscode';
 
@@ -11,38 +12,37 @@ import { TestConfig } from './Models/testConfig';
 import * as Logger from './Utils/Logger/logger';
 
 export class TestConfigManager {
-    constructor(private readonly _configPath: string, private readonly _projectManager: ProjectManager) {
+    private readonly _configPath: string;
+    constructor(private readonly _projectManager: ProjectManager) {
+        const workspaceFolders = workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            throw new Error('Not supported without a folder!');
+        }
+        this._configPath = path.join(workspaceFolders[0].uri.fsPath, '.vscode', Configs.TEST_LAUNCH_CONFIG_NAME);
     }
 
     public get configPath(): string {
         return this._configPath;
     }
 
-    public loadConfig(): Promise<TestConfig> {
-        return new Promise((resolve, reject) => {
-            fs.access(this._configPath, (err) => {
-                if (err) {
-                    const config: TestConfig = this.createDefaultTestConfig();
-                    const content: string = JSON.stringify(config, null, 4);
-                    fs.writeFile(this._configPath, content, 'utf-8', (writeErr) => {
-                        if (writeErr) {
-                            Logger.error('Failed to create default test config!', {
-                                error: writeErr,
-                            });
-                            return reject(writeErr);
-                        }
-                        resolve(config);
+    public async loadConfig(): Promise<TestConfig> {
+        await this.createTestConfigIfNotExisted();
+        return new Promise<TestConfig>((resolve, reject) => {
+            fs.readFile(this._configPath, 'utf-8', (readErr, data) => {
+                if (readErr) {
+                    Logger.error('Failed to read the test config!', {
+                        error: readErr,
                     });
-                } else {
-                    fs.readFile(this._configPath, 'utf-8', (readErr, data) => {
-                        if (readErr) {
-                            Logger.error('Failed to load test config!', {
-                                error: readErr,
-                            });
-                            return reject(readErr);
-                        }
-                        resolve(JSON.parse(data) as TestConfig);
+                    return reject(readErr);
+                }
+                try {
+                    const config: TestConfig = JSON.parse(data) as TestConfig;
+                    resolve(config);
+                } catch (ex) {
+                    Logger.error('Failed to parse the test config!', {
+                        error: ex,
                     });
+                    reject(ex);
                 }
             });
         });
@@ -50,11 +50,39 @@ export class TestConfigManager {
 
     public editConfig(): Promise<TextEditor> {
         const editor = window.activeTextEditor;
-        return this.loadConfig().then(() => {
+        return this.createTestConfigIfNotExisted().then(() => {
             return workspace.openTextDocument(this._configPath).then((doc) => {
                 return window.showTextDocument(doc, editor ? editor.viewColumn : undefined);
             }, (err) => {
                 return Promise.reject(err);
+            });
+        });
+    }
+
+    private createTestConfigIfNotExisted(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            mkdirp(path.dirname(this._configPath), (merr) => {
+                if (merr && merr.code !== 'EEXIST') {
+                    Logger.error(`Failed to create sub directory for this config. File path: ${this._configPath}`, {
+                        error: merr,
+                    });
+                    return reject(merr);
+                }
+                fs.access(this._configPath, (err) => {
+                    if (err) {
+                        const config: TestConfig = this.createDefaultTestConfig();
+                        const content: string = JSON.stringify(config, null, 4);
+                        fs.writeFile(this._configPath, content, 'utf-8', (writeErr) => {
+                            if (writeErr) {
+                                Logger.error('Failed to create default test config!', {
+                                    error: writeErr,
+                                });
+                                return reject(writeErr);
+                            }
+                        });
+                    }
+                    resolve();
+                });
             });
         });
     }
