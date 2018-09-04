f6770c45f58ab73ef84392220e782eba6d418a33
Sheng Chen <sheche@microsoft.com>
2018-09-04 13:44:58 +0800

    update the gulp script to download the language server automatically

--- extension/gulpfile.js
+++ extension/gulpfile.js
@@ -2,15 +2,28 @@ const gulp = require('gulp');
 const cp = require('child_process');
 const tslint = require("gulp-tslint");
 const sass = require('gulp-sass');
-const server_dir = '../';
-gulp.task('build_server', () => {
+const decompress = require('gulp-decompress');
+const download = require('gulp-download');
+const path = require("path");
+
+const server_dir = path.join(__dirname, '..');
+
+gulp.task('build-server', ['build-plugin', 'download-server']);
+
+gulp.task('build-plugin', () => {
     cp.execSync(mvnw() + ' clean package', { cwd: server_dir, stdio: [0, 1, 2] });
-    gulp.src(server_dir + '/com.microsoft.java.test.plugin/target/*.jar')
+    gulp.src(path.join(server_dir, 'com.microsoft.java.test.plugin/target/*.jar'))
         .pipe(gulp.dest('./server'));
-    gulp.src(server_dir + '/com.microsoft.java.test.runner/target/*.jar')
+    gulp.src(path.join(server_dir, 'com.microsoft.java.test.runner/target/*.jar'))
         .pipe(gulp.dest('./server'));
-    gulp.src(server_dir + '/com.microsoft.java.test.runner.junit5/target/*.jar')
+    gulp.src(path.join(server_dir, 'com.microsoft.java.test.runner.junit5/target/*.jar'))
         .pipe(gulp.dest('./server'));
+});
+
+gulp.task('download-server', () => {
+	download("http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz")
+		.pipe(decompress())
+		.pipe(gulp.dest(path.join(server_dir, 'server')));
 });
 
 gulp.task("tslint", () => {
