49a48663f49229a710ee8ae511f15801fc930824
Sheng Chen <sheche@microsoft.com>
2018-09-28 15:16:49 +0800

    add telemetry wrapper (#326)

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,14 +2,20 @@
 // Licensed under the MIT license.
 
 import { commands, ExtensionContext, window } from 'vscode';
+import { initializeFromJsonFile, instrumentOperation } from 'vscode-extension-telemetry-wrapper';
 import { testExplorer } from './explorer/testExplorer';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-    testExplorer.initialize(context);
-    context.subscriptions.push(window.registerTreeDataProvider(testExplorer.testExplorerViewId, testExplorer));
-    await commands.executeCommand('setContext', 'java.test.activated', true);
+    await initializeFromJsonFile(context.asAbsolutePath('./package.json'));
+    await instrumentOperation('activation', doActivate)(context);
 }
 
 export function deactivate(): void {
     // do nothing
+}
+
+async function doActivate(_operationId: string, context: ExtensionContext): Promise<void> {
+    testExplorer.initialize(context);
+    context.subscriptions.push(window.registerTreeDataProvider(testExplorer.testExplorerViewId, testExplorer));
+    await commands.executeCommand('setContext', 'java.test.activated', true);
 }
