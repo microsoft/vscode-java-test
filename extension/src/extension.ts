2b5bc2b13aa2ea80ad01a1b5e9115ef3662b83ed
Sheng Chen <sheche@microsoft.com>
2018-09-29 10:35:13 +0800

    Show the text document when select tree node (#327)

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,9 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { commands, ExtensionContext, window } from 'vscode';
+import { commands, Disposable, ExtensionContext, window } from 'vscode';
 import { initializeFromJsonFile, instrumentOperation } from 'vscode-extension-telemetry-wrapper';
+import { openTextDocumentForNode } from './commands/explorerCommands';
+import { JavaTestRunnerCommands } from './constants/commands';
 import { testExplorer } from './explorer/testExplorer';
+import { TestTreeNode } from './explorer/TestTreeNode';
 
 export async function activate(context: ExtensionContext): Promise<void> {
     await initializeFromJsonFile(context.asAbsolutePath('./package.json'));
@@ -16,6 +19,14 @@ export function deactivate(): void {
 
 async function doActivate(_operationId: string, context: ExtensionContext): Promise<void> {
     testExplorer.initialize(context);
-    context.subscriptions.push(window.registerTreeDataProvider(testExplorer.testExplorerViewId, testExplorer));
+    context.subscriptions.push(
+        window.registerTreeDataProvider(testExplorer.testExplorerViewId, testExplorer),
+        instrumentAndRegisterCommand(JavaTestRunnerCommands.OPEN_DOCUMENT_FOR_NODE, async (node: TestTreeNode) => await openTextDocumentForNode(node)),
+    );
     await commands.executeCommand('setContext', 'java.test.activated', true);
+}
+
+function instrumentAndRegisterCommand(name: string, cb: (...args: any[]) => any): Disposable {
+    const instrumented: (...args: any[]) => any = instrumentOperation(name, async (_operationId: string, ...args: any[]) => await cb(...args));
+    return commands.registerCommand(name, instrumented);
 }
