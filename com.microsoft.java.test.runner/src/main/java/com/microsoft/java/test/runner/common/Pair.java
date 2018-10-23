eef6fbdc02dea1edfddbe19f9217bf10b082883c
Sheng Chen <sheche@microsoft.com>
2018-10-23 16:10:46 +0800

    Add the common logic for the runners (#350)

---
+++ com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/common/Pair.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright (c) 2018 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package com.microsoft.java.test.runner.common;
+
+public final class Pair {
+    public final String first;
+    public final String second;
+
+    public Pair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+}
---
+++ com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/common/TestMessageItem.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+* Copyright (c) 2018 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package com.microsoft.java.test.runner.common;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestMessageItem {
+    String name;
+
+    List<Pair> attributes;
+
+    TestMessageType type;
+
+    public TestMessageItem(TestMessageType type, String name, List<Pair> attributes) {
+        this.type = type;
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    public TestMessageItem(TestMessageType type, String name, Pair... attributes) {
+        this(type, name, attributes != null ? Arrays.asList(attributes) : null);
+    }
+
+    public TestMessageItem(String message, Throwable e) {
+        this(TestMessageType.Error, TestMessageConstants.TEST_RUNNER_ERROR,
+                new Pair(TestMessageConstants.MESSAGE, message),
+                new Pair(TestMessageConstants.DETAILS, getStacktrace(e)));
+    }
+
+    private static String getStacktrace(Throwable throwable) {
+        final StringWriter errors = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(errors));
+        return errors.toString();
+    }
+}
