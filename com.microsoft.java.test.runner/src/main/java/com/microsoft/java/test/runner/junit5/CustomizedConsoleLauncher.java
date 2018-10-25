c6b8d6c574effef5e7119c2d719899afd8b0e688
Sheng Chen <sheche@microsoft.com>
2018-10-25 10:10:27 +0800

    fix checkstyle error

--- com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/junit5/CustomizedConsoleLauncher.java
+++ com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/junit5/CustomizedConsoleLauncher.java
@@ -12,20 +12,20 @@ package com.microsoft.java.test.runner.junit5;
 
 import com.microsoft.java.test.runner.common.ITestLauncher;
 
- import org.junit.platform.console.options.CommandLineOptions;
- import org.junit.platform.console.options.CommandLineOptionsParser;
- import org.junit.platform.console.options.JOptSimpleCommandLineOptionsParser;
+import org.junit.platform.console.options.CommandLineOptions;
+import org.junit.platform.console.options.CommandLineOptionsParser;
+import org.junit.platform.console.options.JOptSimpleCommandLineOptionsParser;
 
 public class CustomizedConsoleLauncher implements ITestLauncher {
 
     @Override
     public int execute(String[] args) {
-         try {
-             final CommandLineOptionsParser parser = new JOptSimpleCommandLineOptionsParser();
-             final CommandLineOptions options = parser.parse(args);
-             return new CustomizedConsoleTestExecutor(options).executeTests();
-         } catch (final Exception exception) {
-             return 1;
-         }
+        try {
+            final CommandLineOptionsParser parser = new JOptSimpleCommandLineOptionsParser();
+            final CommandLineOptions options = parser.parse(args);
+            return new CustomizedConsoleTestExecutor(options).executeTests();
+        } catch (final Exception exception) {
+            return 1;
+        }
     }
 }
