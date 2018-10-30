070a86d3ff3d0e3374896310f79bcc7b8950b251
Sheng Chen <sheche@microsoft.com>
2018-10-30 11:22:51 +0800

    Junit 5 support - Java part (#353)

--- com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/common/TestMessageConstants.java
+++ com.microsoft.java.test.runner/src/main/java/com/microsoft/java/test/runner/common/TestMessageConstants.java
@@ -15,6 +15,8 @@ public class TestMessageConstants {
     public static final String TEST_REPORTER_ATTACHED = "testReporterAttached";
     public static final String ROOT_NAME = "rootName";
     public static final String NAME = "name";
+    public static final String ID = "id";
+    public static final String TYPE = "type";
     public static final String LOCATION = "location";
     public static final String TEST_STARTED = "testStarted";
     public static final String TEST_IGNORED = "testIgnored";
@@ -27,6 +29,7 @@ public class TestMessageConstants {
     public static final String SUITE_TREE_ENDED = "suiteTreeEnded";
     public static final String MESSAGE = "message";
     public static final String DETAILS = "details";
+    public static final String STATUS = "status";
     public static final String TEST_FAILED = "testFailed";
     public static final String TEST_RESULT_SUMMARY = "testSummary";
     public static final String TEST_RUNNER_ERROR = "error";
