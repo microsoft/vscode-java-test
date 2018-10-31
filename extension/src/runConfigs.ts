6ff7ada502ac855a479ae01420fd483f41e63719
Sheng Chen <sheche@microsoft.com>
2018-10-31 13:38:47 +0800

    Add config manager to manage the test config for each run/debug

---
+++ extension/src/runConfigs.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+export interface IExecutionConfig {
+    name: string;
+    projectName: string;
+    workingDirectory: string;
+    args: any[];
+    vmargs: any[];
+    env: { [key: string]: string; };
+    preLaunchTask: string;
+}
+
+export interface IExecutionConfigGroup {
+    default: string;
+    items: IExecutionConfig[];
+}
+
+export interface ITestConfig {
+    run: IExecutionConfigGroup;
+    debug: IExecutionConfigGroup;
+}
