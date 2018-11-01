d3a7d3827a94f0e157a2c3a13d9373a1c75746b9
Sheng Chen <sheche@microsoft.com>
2018-11-01 10:57:38 +0800

    Add Junit4 runner (#365)

---
+++ extension/src/runners/junit4Runner/Junit4RunnerResultAnalyzer.ts
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { BaseRunnerResultAnalyzer } from '../baseRunner/BaseRunnerResultAnalyzer';
+import { ITestResultDetails, TestStatus } from '../models';
+
+const TEST_START: string = 'testStarted';
+const TEST_FAIL: string = 'testFailed';
+const TEST_FINISH: string = 'testFinished';
+
+export class JUnit4RunnerResultAnalyzer extends BaseRunnerResultAnalyzer {
+
+    protected processData(data: string): void {
+        const outputData: IJUnit4TestOutputData = JSON.parse(data) as IJUnit4TestOutputData;
+        switch (outputData.name) {
+            case TEST_START:
+                this.testResults.set(outputData.attributes.name, {
+                    status: undefined,
+                });
+                break;
+            case TEST_FAIL:
+                const failedResult: ITestResultDetails | undefined = this.testResults.get(outputData.attributes.name);
+                if (!failedResult) {
+                    return;
+                }
+                failedResult.status = TestStatus.Fail;
+                failedResult.message = this.decodeContent(outputData.attributes.message);
+                failedResult.details = this.decodeContent(outputData.attributes.details);
+                break;
+            case TEST_FINISH:
+                const finishedResult: ITestResultDetails | undefined = this.testResults.get(outputData.attributes.name);
+                if (!finishedResult) {
+                    return;
+                }
+                if (!finishedResult.status) {
+                    finishedResult.status = TestStatus.Pass;
+                }
+                finishedResult.duration = outputData.attributes.duration;
+                break;
+        }
+    }
+}
+
+interface IJUnit4TestOutputData {
+    type: JUnit4TestOutputType;
+    name: string;
+    attributes: IJUnitTestAttributes;
+}
+
+interface IJUnitTestAttributes  {
+    name: string;
+    duration: string;
+    location: string;
+    message: string;
+    details: string;
+}
+
+enum JUnit4TestOutputType {
+    Info,
+    Error,
+}
