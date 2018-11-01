3ecac9a262cd8f04cd39fb4a6575d9d74b629fd1
Sheng Chen <sheche@microsoft.com>
2018-11-01 11:33:53 +0800

    Add TestNG runner

---
+++ extension/src/runners/testngRunner/TestNGRunner.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { ITestItem } from '../../protocols';
+import { BaseRunner } from '../baseRunner/BaseRunner';
+import { BaseRunnerResultAnalyzer } from '../baseRunner/BaseRunnerResultAnalyzer';
+import { TestNGRunnerResultAnalyzer } from './TestNGRunnerResultAnalyzer';
+
+export class TestNGRunner extends BaseRunner {
+
+    public constructCommandParams(): string[] {
+        return [...super.constructCommandParams(), 'testng', ...this.tests.map((t: ITestItem) => t.fullName)];
+    }
+
+    public getTestResultAnalyzer(): BaseRunnerResultAnalyzer {
+        return new TestNGRunnerResultAnalyzer(this.tests);
+    }
+}
