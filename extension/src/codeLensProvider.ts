5748b427e3d93e7e6fd4210032ef673d4dbf89c0
Sheng Chen <sheche@microsoft.com>
2018-11-01 18:15:15 +0800

    Add view test report entry in code lens provider

--- extension/src/codeLensProvider.ts
+++ extension/src/codeLensProvider.ts
@@ -1,12 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { CancellationToken, CodeLens, CodeLensProvider, TextDocument } from 'vscode';
+import { CancellationToken, CodeLens, CodeLensProvider, Event, EventEmitter, TextDocument, Uri } from 'vscode';
 import { JavaTestRunnerCommands } from './constants/commands';
-import { ITestItem } from './protocols';
+import { ITestItem, TestLevel } from './protocols';
+import { ITestResultDetails, TestStatus } from './runners/models';
+import { testResultManager } from './testResultManager';
 import { searchTestCodeLens } from './utils/commandUtils';
+import { isDarwin } from './utils/platformUtils';
 
 class TestCodeLensProvider implements CodeLensProvider {
+    private onDidChangeCodeLensesEmitter: EventEmitter<void> = new EventEmitter<void>();
+
+    get onDidChangeCodeLenses(): Event<void> {
+        return this.onDidChangeCodeLensesEmitter.event;
+    }
+
+    public refresh(): void {
+        this.onDidChangeCodeLensesEmitter.fire();
+    }
+
     public async provideCodeLenses(document: TextDocument, _token: CancellationToken): Promise<CodeLens[]> {
         try {
             const testItems: ITestItem[] = await searchTestCodeLens(document.uri.toString());
@@ -32,14 +45,15 @@ class TestCodeLensProvider implements CodeLensProvider {
     }
 
     private parseCodeLenses(test: ITestItem): CodeLens[] {
-        return [
+        const codeLenses: CodeLens[] = [];
+        codeLenses.push(
             new CodeLens(
                 test.range,
                 {
                     title: 'Run Test',
                     command: JavaTestRunnerCommands.RUN_TEST_FROM_CODELENS,
                     tooltip: 'Run Test',
-                    arguments: [test],
+                    arguments: [[test]],
                 },
             ),
             new CodeLens(
@@ -48,10 +62,48 @@ class TestCodeLensProvider implements CodeLensProvider {
                     title: 'Debug Test',
                     command: JavaTestRunnerCommands.DEBUG_TEST_FROM_CODELENS,
                     tooltip: 'Debug Test',
-                    arguments: [test],
+                    arguments: [[test]],
                 },
             ),
-        ];
+        );
+
+        if (testResultManager.hasResultWithUri(Uri.parse(test.uri).fsPath)) {
+            codeLenses.push(this.parseCodeLensForTestResult(test));
+        }
+        return codeLenses;
+    }
+
+    private parseCodeLensForTestResult(test: ITestItem): CodeLens {
+        const testMethods: ITestItem[] = [];
+        if (test.level === TestLevel.Method) {
+            testMethods.push(test);
+        } else {
+            testMethods.push(...test.children.map((testChild: ITestItem) => testChild));
+        }
+        return new CodeLens(
+            test.range,
+            {
+                title: this.getTestStatusIcon(testMethods),
+                command: JavaTestRunnerCommands.SHOW_TEST_REPORT,
+                tooltip: 'Show Report',
+                arguments: [testMethods],
+            },
+        );
+    }
+
+    private getTestStatusIcon(testMethods: ITestItem[]): string {
+        for (const method of testMethods) {
+            const testResult: ITestResultDetails | undefined = testResultManager.getResult(Uri.parse(method.uri).fsPath, method.fullName);
+            if (!testResult) {
+                return '❓';
+            } else if (testResult.status === TestStatus.Skip) {
+                return '❔';
+            } else if (testResult.status === TestStatus.Fail) {
+                return '❌';
+            }
+        }
+
+        return isDarwin() ? '✅' : '✔️';
     }
 }
 
