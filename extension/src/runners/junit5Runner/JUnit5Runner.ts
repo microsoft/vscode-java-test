8a243adc6413b61f36844923e2d874ce85072936
Sheng Chen <sheche@microsoft.com>
2018-11-01 11:30:51 +0800

    Add Junit5 runner (#366)

---
+++ extension/src/runners/junit5Runner/JUnit5Runner.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { TestLevel } from '../../protocols';
+import { BaseRunner } from '../baseRunner/BaseRunner';
+import { BaseRunnerResultAnalyzer } from '../baseRunner/BaseRunnerResultAnalyzer';
+import { JUnit5RunnerResultAnalyzer } from './JUnit5RunnerResultAnalyzer';
+
+export class JUnit5Runner extends BaseRunner {
+    public constructCommandParams(): string[] {
+        return [...super.constructCommandParams(), 'junit5', ...this.constructParamsForTests()];
+    }
+
+    public getTestResultAnalyzer(): BaseRunnerResultAnalyzer {
+        return new JUnit5RunnerResultAnalyzer(this.tests);
+    }
+
+    private constructParamsForTests(): string[] {
+        const params: string[] = [];
+        for (const test of this.tests) {
+            if (test.level === TestLevel.Class) {
+                params.push('-c', test.fullName);
+            } else if (test.level === TestLevel.Method) {
+                params.push('-m', test.fullName);
+            }
+        }
+        return params;
+    }
+}
