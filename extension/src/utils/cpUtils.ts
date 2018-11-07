72ae9e553a4023fda78401cb63dcdc9ce1f6f0ec
Sheng Chen <sheche@microsoft.com>
2018-11-07 16:15:22 +0800

    Swallow the error when killing the child process (#403)

--- extension/src/utils/cpUtils.ts
+++ extension/src/utils/cpUtils.ts
@@ -3,15 +3,11 @@
 
 import * as cp from 'child_process';
 import * as kill from 'tree-kill';
-import { testOutputChannel } from '../testOutputChannel';
 
 export function killProcess(process: cp.ChildProcess): Promise<void> {
     return new Promise<void>((resolve: () => void): void => {
-        if (process) {
-            kill(process.pid, 'SIGTERM', (error: Error | undefined) => {
-                if (error) {
-                    testOutputChannel.error('Failed to kill the process', error);
-                }
+        if (process && !process.killed) {
+            kill(process.pid, 'SIGTERM', () => {
                 return resolve();
             });
         }
