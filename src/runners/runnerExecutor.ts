400c0f95b5b59d335fefd3f94dbb0918745e841d
Sheng Chen <sheche@microsoft.com>
2019-01-11 10:12:55 +0800

    Remove unused imports

--- src/runners/runnerExecutor.ts
+++ src/runners/runnerExecutor.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { ExtensionContext, Uri, window, workspace, WorkspaceFolder } from 'vscode';
+import { ExtensionContext, window } from 'vscode';
 import { testCodeLensProvider } from '../codeLensProvider';
 import { logger } from '../logger/logger';
 import { ITestItem, TestKind } from '../protocols';
