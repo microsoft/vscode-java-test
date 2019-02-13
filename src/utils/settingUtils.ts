b7f30090cd84ca70460ab15e51a1fe1a184fa064
Sheng Chen <sheche@microsoft.com>
2019-02-13 16:39:47 +0800

    Support  in all the fields in the test configurations

--- src/utils/settingUtils.ts
+++ src/utils/settingUtils.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import * as _ from 'lodash';
 import { Uri, ViewColumn, workspace, WorkspaceConfiguration, WorkspaceFolder } from 'vscode';
 import { DEFAULT_LOG_LEVEL, DEFAULT_REPORT_POSITION, LOG_LEVEL_SETTING_KEY, REPORT_POSITION_SETTING_KEY } from '../constants/configs';
 import { logger } from '../logger/logger';
+import { IExecutionConfig } from '../runConfigs';
 
 export function getReportPosition(): ViewColumn {
     const config: WorkspaceConfiguration = workspace.getConfiguration();
@@ -16,14 +18,50 @@ export function getLogLevel(): string {
 }
 
 const workspaceRegexp: RegExp = /\$\{workspacefolder\}/i;
-export function resolveWorkingDirectory(testUriString: string, cwd: string | undefined): string | undefined {
-    if (cwd && workspaceRegexp.test(cwd)) {
-        const workspaceFolder: WorkspaceFolder | undefined = workspace.getWorkspaceFolder(Uri.parse(testUriString));
-        if (workspaceFolder) {
-            return cwd.replace(workspaceRegexp, workspaceFolder.uri.fsPath);
-        }
-        logger.error(`Failed to parse the working directory for test: ${testUriString}`);
-        return undefined;
+export function resolve(config: IExecutionConfig, uri: Uri): IExecutionConfig {
+    const resolvedConfig: IExecutionConfig = {};
+    const workspaceFolder: WorkspaceFolder | undefined = workspace.getWorkspaceFolder(uri);
+    if (!workspaceFolder) {
+        logger.error(`Failed to parse the working directory for test: ${uri}`);
+        return config;
     }
-    return cwd;
+
+    if (config.workingDirectory) {
+        if (workspaceRegexp.test(config.workingDirectory)) {
+            resolvedConfig.workingDirectory = config.workingDirectory.replace(workspaceRegexp, workspaceFolder.uri.fsPath);
+        }
+    }
+
+    if (config.args) {
+        resolvedConfig.args = [];
+        for (const arg of config.args) {
+            if (needResolve(arg)) {
+                resolvedConfig.args.push(arg.replace(workspaceRegexp, workspaceFolder.uri.fsPath));
+            }
+        }
+    }
+
+    if (config.vmargs) {
+        resolvedConfig.vmargs = [];
+        for (const vmarg of config.vmargs as string[]) {
+            if (needResolve(vmarg)) {
+                resolvedConfig.vmargs.push(vmarg.replace(workspaceRegexp, workspaceFolder.uri.fsPath));
+            }
+        }
+    }
+
+    if (config.env) {
+        resolvedConfig.env = {};
+        for (const key of Object.keys(config.env)) {
+            if (needResolve(config.env[key])) {
+                resolvedConfig.env[key] = config.env[key].replace(workspaceRegexp, workspaceFolder.uri.fsPath);
+            }
+        }
+    }
+
+    return resolvedConfig;
+}
+
+function needResolve(value: any): boolean {
+    return _.isString(value) && workspaceRegexp.test(value);
 }
