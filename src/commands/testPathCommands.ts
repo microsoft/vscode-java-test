447c8ea301f7d9d898d26d253c10d5b76dff0715
Sheng Chen <sheche@microsoft.com>
2019-05-06 15:28:22 +0800

    Fix lint error

--- src/commands/testPathCommands.ts
+++ src/commands/testPathCommands.ts
@@ -1,19 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { workspace, window, WorkspaceFolder } from "vscode";
-import { getTestSourcePaths } from "../utils/commandUtils";
+import { window, workspace, WorkspaceFolder } from 'vscode';
+import { getTestSourcePaths } from '../utils/commandUtils';
 
 export async function listTestSourcePaths(): Promise<void> {
     if (!workspace.workspaceFolders) {
-        window.showInformationMessage("No workspace opened in VS Code.");
+        window.showInformationMessage('No workspace opened in VS Code.');
         return;
     }
     const sourcePaths: ITestSourcePath[] = await getTestSourcePaths(workspace.workspaceFolders.map((workspaceFolder: WorkspaceFolder) => workspaceFolder.uri.toString()));
     if (sourcePaths.length === 0) {
         window.showInformationMessage("No Java test source directories found in the workspace, please use the command 'Add Folder to Java Test Source Path' first.");
     } else {
-        window.showQuickPick(sourcePaths.map(sourcePath => {
+        window.showQuickPick(sourcePaths.map((sourcePath: ITestSourcePath) => {
             return {
                 label: sourcePath.path,
                 detail: `$(file-directory) ${sourcePath.projectType} Project: ${sourcePath.projectName}`,
