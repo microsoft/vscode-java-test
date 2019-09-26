2669e2d06197cb622a607f3b7c2f56c4ca2192f0
Sheng Chen <sheche@microsoft.com>
2019-09-26 16:07:41 +0800

    fix: Disconnect the debug session when cleaning up

--- src/runners/junit4Runner/Junit4Runner.ts
+++ src/runners/junit4Runner/Junit4Runner.ts
@@ -1,13 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { debug, DebugConfiguration, Uri, workspace } from 'vscode';
+import { debug, DebugConfiguration, DebugSession, Disposable, Uri, workspace } from 'vscode';
 import { logger } from '../../logger/logger';
 import { BaseRunner } from '../baseRunner/BaseRunner';
 import { BaseRunnerResultAnalyzer } from '../baseRunner/BaseRunnerResultAnalyzer';
 import { JUnit4RunnerResultAnalyzer } from './JUnit4RunnerResultAnalyzer';
 
 export class JUnit4Runner extends BaseRunner {
+
+    private debugSession: DebugSession | undefined;
+    private disposables: Disposable[] = [];
+
+    public async tearDown(isCancel: boolean): Promise<void> {
+        super.tearDown(isCancel);
+        if (this.debugSession) {
+            this.debugSession.customRequest('disconnect');
+            this.debugSession = undefined;
+        }
+        for (const disposable of this.disposables) {
+            disposable.dispose();
+        }
+    }
 
     protected get testResultAnalyzer(): BaseRunnerResultAnalyzer {
         if (!this.runnerResultAnalyzer) {
@@ -24,6 +38,11 @@ export class JUnit4Runner extends BaseRunner {
         const uri: Uri = Uri.parse(this.tests[0].location.uri);
         logger.verbose(`Launching with the following launch configuration: '${JSON.stringify(launchConfiguration, null, 2)}'\n`);
         debug.startDebugging(workspace.getWorkspaceFolder(uri), launchConfiguration);
+        this.disposables.push(debug.onDidStartDebugSession((session: DebugSession) => {
+            if (launchConfiguration.name === session.name) {
+                this.debugSession = session;
+            }
+        }));
     }
 }
 
