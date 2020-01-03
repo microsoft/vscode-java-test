c40e5216545eea9824af7386fd194af6a8cfcaac
Sheng Chen <sheche@microsoft.com>
2020-01-03 12:40:04 +0800

    feat: Add more shortcut in Test Explorer

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -20,6 +20,8 @@ export namespace JavaTestRunnerCommands {
     export const RUN_TEST_FROM_CODELENS: string = 'java.test.run';
     export const DEBUG_TEST_FROM_CODELENS: string = 'java.test.debug';
     export const RUN_TEST_FROM_EXPLORER: string = 'java.test.explorer.run';
+    export const DEBUG_ALL_TEST_FROM_EXPLORER: string = 'java.test.explorer.debugAll';
+    export const RUN_ALL_TEST_FROM_EXPLORER: string = 'java.test.explorer.runAll';
     export const DEBUG_TEST_FROM_EXPLORER: string = 'java.test.explorer.debug';
     export const SHOW_TEST_REPORT: string = 'java.test.show.report';
     export const SHOW_TEST_OUTPUT: string = 'java.test.show.output';
