0b09186055c5973996096090be9fc2e26cb4bd1a
Sheng Chen <sheche@microsoft.com>
2020-03-19 16:13:08 +0800

    Strict content security policy (#965)

--- src/testReportProvider.ts
+++ src/testReportProvider.ts
@@ -133,6 +133,7 @@ class TestReportProvider implements Disposable {
             failedCount,
             skippedCount,
             resourceBaseUri: Uri.file(path.join(this.resourceBasePath)).with({ scheme: 'vscode-resource' }),
+            nonce: this.getNonce(),
         });
     }
 
@@ -149,6 +150,15 @@ class TestReportProvider implements Disposable {
         } else {
             map.set(classFullName, [reportItem]);
         }
+    }
+
+    private getNonce(): string {
+        let text: string = '';
+        const possible: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i: number = 0; i < 32; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
     }
 }
 
