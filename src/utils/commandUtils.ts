f27f9e0f1abec716ab66b53cf6c4a429dc500aef
Sheng Chen <sheche@microsoft.com>
2020-10-19 17:06:55 +0800

    bugfix: Ignore error caused by request cancelled (#1083)

--- src/utils/commandUtils.ts
+++ src/utils/commandUtils.ts
@@ -61,7 +61,15 @@ async function executeJavaLanguageServerCommand<T>(...rest: any[]): Promise<T | 
     try {
         return await commands.executeCommand<T>(JavaLanguageServerCommands.EXECUTE_WORKSPACE_COMMAND, ...rest);
     } catch (error) {
+        if (isCancelledError(error)) {
+            return;
+        }
         logger.error(error.toString());
         throw error;
     }
+}
+
+function isCancelledError(error: any): boolean {
+    // See: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#responseMessage
+    return error.code === -32800;
 }
