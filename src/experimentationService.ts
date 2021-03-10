db9cea74baa17cfa6470beab8b190131e5247335
Sheng Chen <sheche@microsoft.com>
2021-03-10 10:32:42 +0800

    Adopt the TAS client

---
+++ src/experimentationService.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { ExtensionContext } from 'vscode';
+import { addContextProperty, sendInfo } from 'vscode-extension-telemetry-wrapper';
+import { getExperimentationService, IExperimentationService, IExperimentationTelemetry, TargetPopulation } from 'vscode-tas-client';
+
+class ExperimentationTelemetry implements IExperimentationTelemetry {
+
+    public setSharedProperty(name: string, value: string): void {
+        addContextProperty(name, value);
+    }
+
+    public postEvent(eventName: string, props: Map<string, string>): void {
+        const payload: any = { __event_name__: eventName };
+        for (const [key, value] of props) {
+            payload[key] = value;
+        }
+
+        sendInfo('', payload);
+    }
+}
+
+let expService: IExperimentationService;
+
+export function getExpService(): IExperimentationService {
+    return expService;
+}
+
+export function initExpService(context: ExtensionContext): void {
+    const packageJson: {[key: string]: any} = require('../package.json');
+    const extensionName: string = `${packageJson['publisher']}.${packageJson['name']}`;
+    const extensionVersion: string = packageJson['version'];
+    expService = getExperimentationService(extensionName, extensionVersion,
+        TargetPopulation.Public, new ExperimentationTelemetry(), context.globalState);
+}
