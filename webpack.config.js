dff84fd05c42ca94486a9c3fccf0d172f89de398
Sheng Chen <sheche@microsoft.com>
2021-04-25 14:55:29 +0800

    chore: Only copy test files in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,50 +6,52 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 /**@type {import('webpack').Configuration}*/
-const config = {
-    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    entry: {
-        "extension.bundle": './extension.bundle.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-    },
-    output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        libraryTarget: "commonjs2",
-        devtoolModuleFilenameTemplate: "../[resource-path]",
-    },
-    externals: {
-        'applicationinsights-native-metrics': 'commonjs applicationinsights-native-metrics', // ignored because we don't ship native module
-        'diagnostic-channel-publishers': 'commonjs diagnostic-channel-publishers',
-        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    },
-    devtool: 'source-map',
-    resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'ts-loader',
+module.exports = (env, options) => {
+    const isProd = options.mode === 'production';
+    return {
+        target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+        entry: {
+            "extension.bundle": './extension.bundle.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+        },
+        output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+            libraryTarget: "commonjs2",
+            devtoolModuleFilenameTemplate: "../[resource-path]",
+        },
+        externals: {
+            'applicationinsights-native-metrics': 'commonjs applicationinsights-native-metrics', // ignored because we don't ship native module
+            'diagnostic-channel-publishers': 'commonjs diagnostic-channel-publishers',
+            vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        },
+        devtool: 'source-map',
+        resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader',
+                }]
             }]
-        }]
-    },
-    plugins: [
-        // Copy files to dist folder where the runtime can find them
-        // @ts-ignore
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.join(__dirname, 'out', 'test'),
-                    to: path.join(__dirname, 'dist', 'test')
-                },
-            ]
-        }),
-    ],
+        },
+        plugins: [
+            // Copy files to dist folder where the runtime can find them
+            // @ts-ignore
+            ...(isProd ? [] : [new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(__dirname, 'out', 'test'),
+                        to: path.join(__dirname, 'dist', 'test')
+                    },
+                ]
+            })]),
+        ],
+    }
 }
-module.exports = config;
