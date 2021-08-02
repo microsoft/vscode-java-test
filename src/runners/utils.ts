fb5c3f53d7560910658f5775a5583a70c669ca63
Sheng Chen <sheche@microsoft.com>
2021-08-02 14:16:37 +0800

    Adopt new testing apis for all the runners

--- src/runners/utils.ts
+++ src/runners/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { Location, Uri } from 'vscode';
+import { Location, TestItem, TestMessage, TestResultState, TestRun, Uri } from 'vscode';
 import { JavaTestRunnerCommands } from '../constants';
 import { asRange } from '../controller/utils';
 import { executeJavaLanguageServerCommand } from '../utils/commandUtils';
@@ -14,4 +14,25 @@ export async function findTestLocation(fullName: string): Promise<Location | und
     }
 
     return undefined;
+}
+
+export function setTestState(testRun: TestRun, item: TestItem, result: TestResultState, message?: TestMessage | TestMessage[], duration?: number): void {
+    switch (result) {
+        case TestResultState.Errored:
+            testRun.errored(item, message || [], duration);
+            break;
+        case TestResultState.Failed:
+            testRun.failed(item, message || [], duration);
+            break;
+        case TestResultState.Passed:
+            testRun.passed(item, duration);
+            break;
+        case TestResultState.Skipped:
+            testRun.skipped(item);
+            break;
+        case TestResultState.Running:
+            testRun.started(item);
+        default:
+            break;
+    }
 }
