f9038e29836d0b930b2a75f8c0e08152dd26a18f
Sheng Chen <sheche@microsoft.com>
2021-11-04 10:31:06 +0800

    feat: Add 'modulePaths' to test configuration
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- src/runConfigs.ts
+++ src/runConfigs.ts
@@ -9,47 +9,62 @@ export interface IExecutionConfig {
      * @since 0.14.0
      */
     name?: string;
+
     /**
      * The working directory when running the tests.
      * @since 0.14.0
      */
     workingDirectory?: string;
+
     /**
-     * The classpath defined in this setting will be appended to the resolved classpath.
+     * The classpaths defined in this setting will be appended to the resolved classpaths.
      * @since 0.33.0
      */
-     classPaths?: string[]
+    classPaths?: string[]
+
+     /**
+      * The modulepaths defined in this setting will be appended to the resolved modulepaths
+      * @since 0.33.0
+      */
+    modulePaths?: string[]
+
     /**
      * The command line arguments which will be passed to the test runner.
      * @since 0.14.0
      */
     args?: any[];
+
     /**
      * the extra options and system properties for the JVM.
      * It's deprecated, we should align with the debug launch configuration, which is 'vmArgs'.
      * @since 0.14.0
      */
     vmargs?: any[];
+
     /**
      * the extra options and system properties for the JVM.
      * @since 0.14.0
      */
     vmArgs?: any[];
+
     /**
      * The extra environment variables when running the tests.
      * @since 0.25.0
      */
     env?: { [key: string]: string; };
+
     /**
      * The absolute path to a file containing environment variable definitions.
      * @since 0.33.0
      */
     envFile?: string;
+
     /**
      * The extra source paths when debugging the tests
      * @since 0.22.4
      */
     sourcePaths?: string[];
+
     /**
      * The label of a task specified in tasks.json.
      * @since 0.33.0
