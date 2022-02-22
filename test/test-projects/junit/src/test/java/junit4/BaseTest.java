ae11aac824ad01790ca203025e1aab5392997b20
Sheng Chen <sheche@microsoft.com>
2022-02-22 16:45:48 +0800

    fix: Find the location of inherited test methods (#1375)
    - When append test messages, keep finding the test method from current
      type to all of its supertype.
    
    Signed-off-by: sheche <sheche@microsoft.com>

---
+++ test/test-projects/junit/src/test/java/junit4/BaseTest.java
@@ -0,0 +1,9 @@
+package junit4;
+
+import org.junit.Test;
+
+public class BaseTest {
+	@Test
+	public void test() {
+	}
+}
---
+++ test/test-projects/junit/src/test/java/junit4/ExtendedTest.java
@@ -0,0 +1,9 @@
+package junit4;
+
+import org.junit.Test;
+
+public class ExtendedTest extends BaseTest {
+	@Test
+	public void extendedTest() {
+	}
+}
