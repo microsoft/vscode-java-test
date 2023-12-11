75845d6d55bbeb761d00155e40b0b11d15e8b5f6
Sheng Chen <sheche@microsoft.com>
2023-12-11 18:17:04 +0800

    init

---
+++ java-extension/com.microsoft.java.test.plugin/src/main/java/com/microsoft/java/test/plugin/coverage/model/MethodCoverage.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+* Copyright (c) 2023 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package com.microsoft.java.test.plugin.coverage.model;
+
+public class MethodCoverage {
+    int lineNumber;
+    int hit;
+    String name;
+
+    public MethodCoverage(int lineNumber, int hit, String name) {
+        this.lineNumber = lineNumber;
+        this.hit = hit;
+        this.name = name;
+    }
+}
